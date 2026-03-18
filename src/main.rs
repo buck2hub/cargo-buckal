1f134cba12d4f78fe879564abd217a9838b63a6a
jl.jiang <jiangjl9807@gmail.com>
2026-03-18 16:30:37 +0800

    chore: update default bundle hash
    Signed-off-by: jl.jiang <jiangjl9807@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ pub const RUST_CRATES_ROOT: &str = "third-party/rust/crates";
 pub const RUST_GIT_ROOT: &str = "third-party/rust/git";
 pub const BUCKAL_BUNDLES_REPO: &str = "buck2hub/buckal-bundles";
 // fallback commit hash used when fetching the latest from BUCKAL_BUNDLES_REPO fails
-pub const DEFAULT_BUNDLE_HASH: &str = "b8ec5f1bb851ef6e572acdcd912b82f3edbfb932";
+pub const DEFAULT_BUNDLE_HASH: &str = "8d30857f71a6cfcc84e486ff326b990af9212084";
 
 pub fn main() {
     let args = cli::Cli::parse();
